863fef597fc1ba037789c6cdbbc2a7b15dfd4983
Masamitsu MURASE <masamitsu.murase@gmail.com>
2019-12-17 00:28:18 +0900

    Initial commit 2.

--- src/js/content_script/main.js
+++ src/js/content_script/main.js
@@ -1,35 +1,39 @@
 
 (function (ctx) {
-    const DECORATOR_CLASS = "redmine-decorator";
+    const DECORATOR_CLASS = "murase-redmine-decorator";
     const CHANGE_TO_CLOSED_LINK = {
         "class": ["icon", "icon-closed"],
         "text": chrome.i18n.getMessage("closed"),
         "next_sibling_selector": "a.icon.icon-edit"
     };
+    const STATUS_RESOLVED = 3;
     const STATUS_CLOSED = 5;
-    const API_KEY = "28a16b54caa3f7ba85a2c16e3818275153a5635b";
-    // const API_KEY = "4c5454d28911872c291251438ebf333447c9f259";
-    const REDMINE_BASE_URL = "http://localhost:3000/";
+    const STATUS_SELECT_ID = "issue_status_id";
+    const ASSIGNEE_SELECT_ID = "issue_assigned_to_id";
+    const ASSIGNEE_OPTGROUP_LABEL = "企画部";
+    const ASSIGNEE_FOR_RESOLVED = "末永 文";
+    const API_KEY = "fafd1a57c06bf5d9ea2643c33f692e656316f34f";
+    const REDMINE_BASE_URL = "https://demo.lychee-redmine.jp/";
     const API_HEADER_KEY = "X-Redmine-API-Key";
 
-    var isIssuePage = function (url) {
+    let isIssuePage = function (url) {
         if (!url.startsWith(REDMINE_BASE_URL)) {
             return false;
         }
 
-        if (!url.substr(REDMINE_BASE_URL.length).match(/^issues\/[0-9]+/)) {
+        if (!url.substr(REDMINE_BASE_URL.length).match(/^issues\/[0-9]+\/?$/)) {
             return false;
         }
 
         return true;
     };
 
-    var issueId = function (url) {
+    let issueId = function (url) {
         if (!isIssuePage(url)) {
             throw `Invalid URL ${url}`
         }
 
-        var match_data = url.substr(REDMINE_BASE_URL.length).match(/^issues\/([0-9]+)/);
+        let match_data = url.substr(REDMINE_BASE_URL.length).match(/^issues\/([0-9]+)/);
         if (!match_data) {
             throw `Invalid URL ${url}`;
         }
@@ -37,11 +41,11 @@
         return parseInt(match_data[1], 10);
     };
 
-    var issueUrl = function (issue_id, format = ".json") {
+    let issueUrl = function (issue_id, format = ".json") {
         return `${REDMINE_BASE_URL}issues/${issue_id}${format}`;
     };
 
-    var canChangeToClosed = function (issue_info) {
+    let canChangeToClosed = function (issue_info) {
         if (!issue_info["assigned_to"]) {
             return false;
         }
@@ -49,58 +53,58 @@
         return true;
     };
 
-    var createLink = function (text, class_list) {
-        var elem = document.createElement("a");
+    let createLink = function (text, class_list) {
+        let elem = document.createElement("a");
         elem.href = "#";
         elem.className = class_list.concat(DECORATOR_CLASS).join(" ");
         elem.innerText = text;
         return elem;
     };
 
-    var createChangeToClosedLink = function () {
+    let createChangeToClosedLink = function () {
         return createLink(CHANGE_TO_CLOSED_LINK["text"], CHANGE_TO_CLOSED_LINK["class"]);
     };
 
-    var getCurrentIssueInfo = async function () {
-        var url = location.href;
-        var issue_id = issueId(url);
-        var issue_url = issueUrl(issue_id);
-        var headers = {};
+    let getCurrentIssueInfo = async function () {
+        let url = location.href;
+        let issue_id = issueId(url);
+        let issue_url = issueUrl(issue_id);
+        let headers = {};
         headers[API_HEADER_KEY] = API_KEY;
-        var issue_info = await ctx.Misc.getJson(issue_url, headers);
+        let issue_info = await ctx.Misc.getJson(issue_url, headers);
         return issue_info["issue"];
     };
 
-    var changeIssueStatusToClosed = async function () {
-        var url = location.href;
-        var issue_id = issueId(url);
-        var issue_url = issueUrl(issue_id);
-        var issue_info = {
+    let changeIssueStatusToClosed = async function () {
+        let url = location.href;
+        let issue_id = issueId(url);
+        let issue_url = issueUrl(issue_id);
+        let issue_info = {
             "issue": {
                 "status_id": STATUS_CLOSED,
                 // "assigned_to_id": 
             }
         };
-        var headers = {};
+        let headers = {};
         headers[API_HEADER_KEY] = API_KEY;
         await ctx.Misc.putJson(issue_url, issue_info, headers);
     };
 
-    var reloadPage = function () {
+    let reloadPage = function () {
         location.reload(true);
     };
 
-    var eventHandlerForClosedLink = function (event) {
+    let eventHandlerForClosedLink = function (event) {
         (async function () {
             try {
-                var current_issue_info = await getCurrentIssueInfo();
+                let current_issue_info = await getCurrentIssueInfo();
                 if (!canChangeToClosed(current_issue_info)) {
                     alert(chrome.i18n.getMessage("cannot_change_to_closed"));
                     return;
                 }
 
                 await changeIssueStatusToClosed();
-                var new_issue_info = await getCurrentIssueInfo();
+                let new_issue_info = await getCurrentIssueInfo();
                 if (new_issue_info["status"]["id"] != STATUS_CLOSED) {
                     alert(chrome.i18n.getMessage("failed_to_change_to_closed"));
                     return;
@@ -115,36 +119,63 @@
         event.preventDefault();
     };
 
-    var addEventListenerToClosedLink = function (elem) {
+    let addEventListenerToClosedLink = function (elem) {
         elem.addEventListener("click", eventHandlerForClosedLink);
     };
 
-    var addChangeToClosedLinks = function () {
-        var base_elems = Array.from(document.querySelectorAll(CHANGE_TO_CLOSED_LINK["next_sibling_selector"]));
+    let addChangeToClosedLinks = function () {
+        let base_elems = Array.from(document.querySelectorAll(CHANGE_TO_CLOSED_LINK["next_sibling_selector"]));
         if (base_elems.length === 0) {
             console.log("No links are found.");
             return;
         }
 
         base_elems.forEach((base_elem) => {
-            var link_elem = createChangeToClosedLink();
+            let link_elem = createChangeToClosedLink();
             base_elem.parentNode.insertBefore(link_elem, base_elem);
             addEventListenerToClosedLink(link_elem);
         });
     };
 
-    var insertAdditionalLinks = function () {
+    let insertAdditionalLinks = function () {
         addChangeToClosedLinks();
     };
 
-    var removeAdditionalLinks = function () {
-        var elems = Array.from(document.querySelectorAll("." + DECORATOR_CLASS));
+    let removeAdditionalLinks = function () {
+        let elems = Array.from(document.querySelectorAll("." + DECORATOR_CLASS));
         elems.forEach(elem => elem.parentNode.removeChild(elem));
+    };
+
+    let addAssigneeSynchronizer = function () {
+        let option_selector = `optgroup[Label="${ASSIGNEE_OPTGROUP_LABEL}"] option`;
+        document.body.addEventListener("change", function (event) {
+            let status_select = event.target;
+            if (!status_select || status_select.id != STATUS_SELECT_ID) {
+                return;
+            }
+
+            if (status_select.value != STATUS_RESOLVED) {
+                return;
+            }
+
+            let assignee_select = document.querySelector(`#${ASSIGNEE_SELECT_ID}`);
+            let assignee_option = Array.from(assignee_select.querySelectorAll(option_selector))
+                .find(elem => elem.textContent.trim() == ASSIGNEE_FOR_RESOLVED);
+            if (!assignee_option) {
+                return;
+            }
+
+            assignee_select.selectedIndex = assignee_option.index;
+            let change_event = document.createEvent("HTMLEvents");
+            change_event.initEvent("change", true, true);
+            assignee_select.dispatchEvent(change_event);
+        });
     };
 
 
     if (isIssuePage(location.href)) {
         removeAdditionalLinks();
         insertAdditionalLinks();
+        addAssigneeSynchronizer();
     }
 })(RedmineDecorator);
